4a1ab6b615589b599ed57ede0cc49edf0222c00d
Sumana Kumbhakar <sumanakumbhakar11@gmail.com>
2025-06-04 14:48:07 +0530

    Add day1_hello program

---
+++ day1_hello.py
@@ -0,0 +1,5 @@
+# Ask for the user's name                       name = input("What is your name?")        
+
+# Ask how they feel                             feeling = input("How are you feeling today")                        
+
+# Print a greeting message                      print("Hello", name + "!")                print("It's great to hear you're feeling", feeling + ".")
